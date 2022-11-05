81094ff2ccdef36f309e4086800f527ca86480d1
Nj180280 <97845995+Nj180280@users.noreply.github.com>
2022-11-05 21:03:29 +0530

    Add files via upload

---
+++ exp_handle1.java
@@ -0,0 +1,10 @@
+//exception handle 1
+import java.util.*; 
+class exp_handle1
+{
+public static void main(String args[])
+{
+int a=10/0;
+System.out.println("when 10 is divided by zero answer is:"+a);  //throws exception
+}
+}
---
+++ exp_handle3.java
@@ -0,0 +1,26 @@
+//exception handle 1
+import java.util.*; 
+class exp_handle3
+{
+public static void main(String args[])
+{
+try
+{
+int a=10/0;
+int b[]=new int[5]; 
+System.out.println("remaining part"); //throws exception
+}
+catch(ArithmeticException e)
+{
+System.out.println(e); 
+}
+catch(ArrayIndexOutOfBoundsException e1)
+{
+System.out.println(e1); 
+}
+finally
+{
+System.out.println("this is executable pa");
+}
+}
+}
